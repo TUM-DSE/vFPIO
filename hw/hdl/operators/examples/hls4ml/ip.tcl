7800252d962b3ac77cd172020c0eddd7a841301c
jedichen121 <chenjiyang1991@gmail.com>
2024-01-07 17:14:31 +0000

    Add benchmarks to comparison branch.

---
+++ hw/hdl/operators/examples/hls4ml/ip.tcl
@@ -0,0 +1,30 @@
+file mkdir $build_dir/iprepo
+set cmd "exec unzip $hw_dir/hdl/operators/examples/hls4ml/dse_in_tum_de_hls4ml_myproject_*.zip -d $build_dir/iprepo/hls4ml_ip"
+eval $cmd
+update_ip_catalog -rebuild
+
+
+	
+set axis_dwidth_converter_c1 [create_ip -name axis_dwidth_converter -vendor xilinx.com -library ip -version 1.1 -module_name axis_dwidth_converter_c1]
+
+# User Parameters
+set_property -dict [list \
+    CONFIG.M_TDATA_NUM_BYTES {6} \
+    CONFIG.S_TDATA_NUM_BYTES {64} \
+] [get_ips axis_dwidth_converter_c1]
+
+# Runtime Parameters
+set_property -dict { 
+    GENERATE_SYNTH_CHECKPOINT {1}
+} $axis_dwidth_converter_c1
+
+# move_files -of_objects [get_reconfig_modules design_user_wrapper_c5_$j] [get_files axis_dwidth_converter_c1.xci]
+
+
+set hls4ml_0 [create_ip -name myproject -vendor dse.in.tum.de -library hls4ml -version 0.2 -module_name hls4ml_0]
+
+set_property -dict { 
+    GENERATE_SYNTH_CHECKPOINT {1}
+} $hls4ml_0
+
+		
---
+++ hw/hdl/operators/examples/rdma_nw/rdma_nw_c0_0.svh
@@ -0,0 +1,54 @@
+// I/O
+always_comb axi_ctrl.tie_off_s();
+
+// UL
+`ifdef EN_RDMA_0
+
+`META_ASSIGN(rdma_0_rd_req, bpss_rd_req)
+`META_ASSIGN(rdma_0_wr_req, bpss_wr_req)
+
+`ifndef EN_MEM
+// One 'axis_rdma_0_sink' line and one 'axis_host_0_sink' line should be uncommented
+`AXISR_ASSIGN(axis_rdma_0_sink, axis_rdma_0_sink2) // RDMA => user logic => host
+//`AXISR_ASSIGN(axis_rdma_0_sink, axis_host_0_src) // RDMA => host
+// `AXISR_ASSIGN(axis_host_0_sink, axis_host_sink2)   // Host => user logic => RDMA
+`AXISR_ASSIGN(axis_host_0_sink, axis_rdma_0_src) // Host => RDMA
+`else
+`AXISR_ASSIGN(axis_rdma_0_sink, axis_card_0_src)
+`AXISR_ASSIGN(axis_card_0_sink, axis_rdma_0_src)
+`endif
+
+`else
+`ifdef EN_RDMA_1
+
+`META_ASSIGN(rdma_1_rd_req, bpss_rd_req)
+`META_ASSIGN(rdma_1_wr_req, bpss_wr_req)
+
+`ifndef EN_MEM
+`AXISR_ASSIGN(axis_rdma_1_sink, axis_host_0_src)
+`AXISR_ASSIGN(axis_host_0_sink, axis_rdma_1_src)
+`else
+`AXISR_ASSIGN(axis_rdma_1_sink, axis_card_0_src)
+`AXISR_ASSIGN(axis_card_0_sink, axis_rdma_1_src)
+`endif
+
+`endif
+`endif
+
+AXI4SR axis_rdma_0_sink2();
+
+// I/O
+AXI4SR axis_sink_int ();
+AXI4SR axis_src_int ();
+
+axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_rdma_0_sink2), .m_axis(axis_sink_int));
+axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_host_0_src));
+
+
+
+nw_axis_wrapper nw_inst (
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .axis_in(axis_sink_int),
+    .axis_out(axis_src_int)
+);
