2b781e3f4b56a9d4867ba1767ca29ec680b8aa87
jedichen121 <chenjiyang1991@gmail.com>
2024-01-10 14:14:25 +0000

    HW examples, including rdma ones.

---
+++ hw/hdl/operators/examples/rdma_nw/rdma_nw_c0_0.svh
@@ -0,0 +1,34 @@
+// I/O
+AXI4SR axis_sink_int ();
+AXI4SR axis_src_int ();
+
+always_comb axi_ctrl.tie_off_s();
+
+axisr_reg_rtl inst_reg_slice_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_switch_mem_0_sink), .m_axis(axis_sink_int));
+axisr_reg_rtl inst_reg_slice_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_switch_mem_0_src));
+
+
+`ifdef EN_RDMA_0
+metaIntf #(.STYPE(req_t)) bpss_tmp();
+assign bpss_tmp.valid                   = rdma_0_wr_req.valid;
+assign rdma_0_wr_req.ready              = bpss_tmp.ready;
+
+assign bpss_tmp.data.vaddr = rdma_0_wr_req.data.vaddr;
+assign bpss_tmp.data.len = 16384;
+assign bpss_tmp.data.stream = rdma_0_wr_req.data.stream;
+assign bpss_tmp.data.sync = rdma_0_wr_req.data.sync;
+assign bpss_tmp.data.ctl = rdma_0_wr_req.data.ctl;
+assign bpss_tmp.data.host = rdma_0_wr_req.data.host;
+assign bpss_tmp.data.dest = rdma_0_wr_req.data.dest;
+assign bpss_tmp.data.pid = rdma_0_wr_req.data.pid;
+assign bpss_tmp.data.vfid = rdma_0_wr_req.data.vfid;
+assign bpss_tmp.data.rsrvd = rdma_0_wr_req.data.rsrvd;
+`endif
+
+
+nw_axis_wrapper nw_inst (
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .axis_in(axis_sink_int),
+    .axis_out(axis_src_int)
+);
--- hw/hdl/operators/examples/rng/rng_c0_0.svh
+++ hw/hdl/operators/examples/rng/rng_c0_0.svh
@@ -5,18 +5,8 @@ AXI4SR axis_src_int ();
 
 always_comb axi_ctrl.tie_off_s();
 
-`ifdef EN_STRM
-    `ifdef EN_MEM
-        axisr_reg_rtl inst_reg_slice_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_switch_mem_0_sink), .m_axis(axis_sink_int));
-        axisr_reg_rtl inst_reg_slice_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_switch_mem_0_src));
-    `else
-		axisr_reg_rtl inst_reg_slice_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_host_0_sink), .m_axis(axis_sink_int));
-        axisr_reg_rtl inst_reg_slice_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_host_0_src));
-    `endif
-`else
-axisr_reg_rtl inst_reg_slice_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_card_0_sink), .m_axis(axis_sink_int));
-axisr_reg_rtl inst_reg_slice_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_card_0_src));
-`endif
+axisr_reg_rtl inst_reg_slice_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_switch_mem_0_sink), .m_axis(axis_sink_int));
+axisr_reg_rtl inst_reg_slice_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_switch_mem_0_src));
 
 
 rng_axis_wrapper rng_inst (
