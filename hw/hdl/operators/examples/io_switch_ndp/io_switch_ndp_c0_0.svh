7cbc2710554132b3d1d15e69cfc7290aece19dcf
jedichen121 <chenjiyang1991@gmail.com>
2024-05-23 07:33:43 +0000

    update io_switch_ndp example

--- hw/hdl/operators/examples/io_switch_ndp/io_switch_ndp_c0_0.svh
+++ hw/hdl/operators/examples/io_switch_ndp/io_switch_ndp_c0_0.svh
@@ -3,17 +3,17 @@ always_comb axi_ctrl.tie_off_s();
 // always_comb axis_card_0_src.tie_off_m();
 // always_comb axis_host_0_sink.tie_off_s();
 
-// I/O
-AXI4SR axis_sink_int ();
-AXI4SR axis_src_int ();
+// // I/O
+// AXI4SR axis_sink_int ();
+// AXI4SR axis_src_int ();
 
-`ifdef EN_STRM
-axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_host_0_sink), .m_axis(axis_sink_int));
-axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_host_0_src));
-`else
-axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_card_0_sink), .m_axis(axis_sink_int));
-axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_card_0_src));
-`endif
+// `ifdef EN_STRM
+// axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_host_0_sink), .m_axis(axis_sink_int));
+// axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_host_0_src));
+// `else
+// axisr_reg inst_reg_sink (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_card_0_sink), .m_axis(axis_sink_int));
+// axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .m_axis(axis_card_0_src));
+// `endif
 
 // UL
 // assign axis_src_int.tvalid  = 1;
@@ -24,11 +24,17 @@ axisr_reg inst_reg_src (.aclk(aclk), .aresetn(aresetn), .s_axis(axis_src_int), .
 // assign axis_src_int.tid     = 0;
 // assign axis_src_int.tlast   = 1;
 
-always_comb begin
-    axis_src_int.tvalid  = axis_sink_int.tvalid;
-    axis_src_int.tdata = axis_sink_int.tdata + 1;
-    axis_src_int.tkeep   = axis_sink_int.tkeep;
-    axis_src_int.tid     = axis_sink_int.tid;
-    axis_src_int.tlast   = axis_sink_int.tlast;
-    axis_sink_int.tready = axis_src_int.tready;
-end
+`AXISR_ASSIGN(axis_card_0_sink, axis_card_0_src)
+`AXISR_ASSIGN(axis_host_0_sink, axis_host_0_src)
+
+`AXISR_ASSIGN(axis_rdma_0_sink, axis_rdma_0_src)
+`AXISR_ASSIGN(axis_rdma_1_sink, axis_rdma_1_src)
+
+// always_comb begin
+//     axis_src_int.tvalid  = axis_sink_int.tvalid;
+//     axis_src_int.tdata = axis_sink_int.tdata + 1;
+//     axis_src_int.tkeep   = axis_sink_int.tkeep;
+//     axis_src_int.tid     = axis_sink_int.tid;
+//     axis_src_int.tlast   = axis_sink_int.tlast;
+//     axis_sink_int.tready = axis_src_int.tready;
+// end
